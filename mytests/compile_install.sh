a03ceb9b3bb82dc145811e017ce3b56b53b6a992
srmarcballestero <marconiballestero@gmail.com>
2025-11-18 11:24:35 +0100

    modify the compile_install command to allow for the rebuild of geant4

--- mytests/compile_install.sh
+++ mytests/compile_install.sh
@@ -4,6 +4,8 @@ src_dir="$HOME/GitHub/OpenTOPAS"
 extensions_flag=""
 build_name="build"
 
+geant4_install_dir="$HOME/Applications/GEANT4/geant4-install"
+
 while [[ $# -gt 0 ]]; do
     case $1 in
         --extensions-dir)
@@ -14,6 +16,16 @@ while [[ $# -gt 0 ]]; do
             build_name="$2"
             shift 2
             ;;
+        --rebuild-geant4)
+            echo "Rebuilding Geant4..."
+            mkdir -p $HOME/GitHub/geant4-v11.1.3/builds/$build_name
+            mkdir -p $HOME/GitHub/geant4-v11.1.3/installs/$build_name
+            cd $HOME/GitHub/geant4-v11.1.3/builds/$build_name
+            cmake $HOME/GitHub/geant4-v11.1.3 -DGEANT4_INSTALL_DATA=OFF -DGEANT4_BUILD_MULTITHREADED=ON -DCMAKE_INSTALL_PREFIX=$HOME/GitHub/geant4-v11.1.3/installs/$build_name -DCMAKE_PREFIX_PATH=/usr/lib/qt5 -DGEANT4_USE_QT=ON -DGEANT4_USE_OPENGL_X11=ON -DGEANT4_USE_RAYTRACER_X11=ON 
+            make -j20 install
+            geant4_install_dir="$HOME/GitHub/geant4-v11.1.3/installs/$build_name"
+            shift
+            ;;
         *)
             echo "Unknown option $1"
             exit 1
@@ -21,7 +33,11 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+export Geant4_DIR=$geant4_install_dir
+export GDCM_DIR=$HOME/Applications/GDCM/gdcm-install
+
 cd $src_dir
+
 
 mkdir -p builds/$build_name
 mkdir -p installs/$build_name
@@ -30,9 +46,6 @@ rm -rf builds/$build_name/*
 rm -rf installs/$build_name/*
 
 cd builds/$build_name
-
-export Geant4_DIR=$HOME/Applications/GEANT4/geant4-install
-export GDCM_DIR=$HOME/Applications/GDCM/gdcm-install
 
 if ! cmake ../.. -DCMAKE_INSTALL_PREFIX=../../installs/$build_name $extensions_flag; then
     echo "Error: CMake configuration failed"
@@ -53,10 +66,10 @@ cat <<EOL > "../../installs/$build_name/setup_env.sh"
 topas_dir="\$HOME/GitHub/OpenTOPAS/installs/$build_name"
 
 export TOPAS_G4_DATA_DIR="\$HOME/Applications/GEANT4/G4DATA"
-if [ -d "\$HOME/Applications/GEANT4/geant4-install/lib64" ]; then
-    export LD_LIBRARY_PATH="\$HOME/Applications/GEANT4/geant4-install/lib64:\$LD_LIBRARY_PATH"
+if [ -d "$Geant4_DIR/lib64" ]; then
+    export LD_LIBRARY_PATH="$Geant4_DIR/lib64:\$LD_LIBRARY_PATH"
 else
-    export LD_LIBRARY_PATH="\$HOME/Applications/GEANT4/geant4-install/lib:\$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="$Geant4_DIR/lib:\$LD_LIBRARY_PATH"
 fi
 
 export QT_QPA_PLATFORM_PLUGIN_PATH="\$topas_dir/Frameworks"
